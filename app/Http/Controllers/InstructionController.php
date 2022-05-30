417fde2d15b75f8c2b7c73952e361562bdac62f4
DevaZikrullah <zdeva804@gmail.com>
2022-05-30 14:54:24 +0700

    feature create

--- app/Http/Controllers/InstructionController.php
+++ app/Http/Controllers/InstructionController.php
@@ -3,25 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Instruction;
+use App\Services\InstructionServices;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class InstructionController extends Controller
 {
+    protected $instructionServices;
+
+        public function __construct(InstructionServices $instructionServices)
+        {
+            $this->instructionServices = $instructionServices;
+        }
+
     public function store(Request $request)
     {
+        
         $validator = Validator::make($request->all(),[
             'instruction_type' => 'required',
-            'vendor_name' => 'required',
-            'vendor_addres' => 'required',
+            'associates.vendor_name' => 'required',
+            'associates.vendor_addres' => 'required',
             'attention_of' => 'required',
             'quatation_of' => 'required',
             'invoice.name' => 'required',
             'invoice.status',
+            'associates.customer_contract'=>'required',
+            'associates.customer_po_no'=>'required',
             'desc' => 'required',
             'qty' => 'required',
             'uom' => 'required',
             'unit_price' => 'required',
+            'disc',
             'tax',
             'invoice.total',
             'charge' => 'required',
@@ -37,20 +49,25 @@ class InstructionController extends Controller
             return response()->json($validator->errors());       
         }
 
+        //membuat data instructions
+
         $instruction = Instruction::create([
             'instruction_type' => $request->instruction_type,
-            'vendor_name' => $request->vendor_name,
-            'vendor_addres' => $request->vendor_addres,
+            'associates.vendor_name' => $request->vendor_name,
+            'associates.vendor_addres' => $request->vendor_addres,
             'attention_of' => $request->attention_of,
             'quatation_no' => $request->quatation_no,
             'invoice.name' => $request->invoice_name,
             'invoice.status' => "progress",
+            'associates.customer_contract'=>$request->customer_contract,
+            'associates.customer_po_no'=>$request->customer_po_no,
             'desc' => $request->desc,
             'qty' => $request->qty,
             'uom' => $request->uom,
             'unit_price' => $request->unit_price,
+            'disc' => $request->disc,
             'tax' => $request->tax,
-            'invoice.total' => $request->qty*$request->unit_price,
+            'invoice.total' => ($total = $request->qty*$request->unit_price)+($total*$request->tax/100)-($total*$request->disc/100),
             'charge' => $request->charge,
             'notes' => $request->notes,
             'attachtment' => $request->attachtment,
@@ -61,13 +78,12 @@ class InstructionController extends Controller
          if ($file = $request->file('attachtment')) 
             $path = $file->store('public/files'); 
             $name = $file->getClientOriginalName();
-            //store your file into directory and db
             $save = new Instruction();
             $save->$name = $file;
             $save-> store_path = $path;
             $save->save();
         
             
-            return response()->json(["created succses",$file]);    
+            return response()->json(["created instruction succses",$instruction]);    
         }
 }
