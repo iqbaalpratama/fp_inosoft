3d6d112fdb4a7fd7ed93fa763b5845db4a001f2e
AdrianID <adrianfrizna14@gmail.com>
2022-06-02 04:04:49 +0700

    Added Material Design Icon

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import config from '../config'
 import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+      },
     theme: {
         themes: {
             light: config.light
