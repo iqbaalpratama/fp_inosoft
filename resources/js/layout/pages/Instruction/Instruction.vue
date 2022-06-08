f33e9866e981c3ebb24d70d6494dbec57ad27a23
AdrianID <adrianfrizna14@gmail.com>
2022-06-08 22:42:11 +0700

    Updated Pages Title

--- resources/js/layout/pages/Instruction/Instruction.vue
+++ resources/js/layout/pages/Instruction/Instruction.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid class="pr-15 pl-15 pt-10">
-    <div class="text-h4">3rd Instruction</div>
+     <div class="text-h4">3rd Party Instruction</div>
 
     <v-breadcrumbs :items="items" class="pl-0 pt-1 mb-10">
       <template v-slot:divider>
@@ -8,8 +8,7 @@
       </template>
     </v-breadcrumbs>
 
-    <v-card class="employee-list pl-3 pr-3">
-    
+    <v-card class="pl-3 pr-3">    
       <v-tabs background-color="accent-4" color="primary" class="mb-5">
         <v-tab :to="{ name: 'TableOpen'}">Open</v-tab>
         <v-tab :to="{ name: 'TableComplete'}">Complete</v-tab>
