91dd8dd3f8bf9fc1bcae6109c2bb2b2332599546
AdrianID <adrianfrizna14@gmail.com>
2022-06-09 17:52:04 +0700

    Setup Store & Service Structure

---
+++ resources/js/services/instruction.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+const API_URL = 'http://localhost:8000/api/';
+
+class InstructionService{
+    
+}
+export default new InstructionService();
---
+++ resources/js/store/instruction.module.js
@@ -0,0 +1,18 @@
+import { InstructionService } from '../services/instruction.service';
+
+const state = {
+};
+
+const actions = {
+};
+
+const mutations = {
+    
+};
+
+export const instruction = {
+    namespaced: true,
+    state,
+    actions,
+    mutations
+};
