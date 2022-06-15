6a440b027abbb99f83017b7dfa0cb6ded0b68ae8
iqbaalpratama <iqbaalpratama23@gmail.com>
2022-06-15 21:39:31 +0700

    merge with main

--- app/Services/InstructionServices.php
+++ app/Services/InstructionServices.php
@@ -15,29 +15,37 @@ class InstructionServices
         $this->instructionRepository = $instructionRepository;
     }
 
+    public function getById($id)
+    {
+        return $this->instructionRepository->getById($id);
+    }
+
+
     public function saveInstruction($data)
     {
 
         $validator = Validator::make($data,[
+            'instruction_id',
             'instruction_type' => 'required',
-            'associates.vendor_name' => 'required',
-            'associates.vendor_addres' => 'required',
+            'associates_vendor_name' => 'required',
+            'associates_vendor_addres' => 'required',
             'attention_of' => 'required',
             'quatation_no' => 'required',
-            'invoice.name' => 'required',
-            'invoice.status',
-            'associates.customer_contract'=>'required',
-            'associates.customer_po_no'=>'required',
+            'invoice_name' => 'required',
+            'invoice_status',
+            'associates_customer_contract'=>'required',
+            'associates_customer_po_no'=>'required',
             'desc' => 'required',
             'qty' => 'required',
             'uom' => 'required',
             'unit_price' => 'required',
             'disc',
             'tax',
+            'curenncy' => 'required',
             'invoice.total',
             'charge' => 'required',
             'notes',
-            'attachtment' => 'required|mimes:doc,docx,pdf,txt,csv|max:2048',
+            'attachtment',
             'link' => 'required'
 
         ]);
@@ -51,6 +59,47 @@ class InstructionServices
         $result = $this->instructionRepository->addInstruction($data);
 
         return $result;
+    }
+
+    public function editInstruction($id,$data)
+    {
+        $validator = Validator::make($data,[
+            'associates_vendor_name' => 'required',
+            'associates_vendor_addres' => 'required',
+            'attention_of' => 'required',
+            'quatation_no' => 'required',
+            'invoice_name' => 'required',
+            'invoice_status',
+            'associates_customer_contract'=>'required',
+            'associates_customer_po_no'=>'required',
+            'desc' => 'required',
+            'qty' => 'required',
+            'uom' => 'required',
+            'unit_price' => 'required',
+            'disc',
+            'tax',
+            'curenncy' => 'required',
+            'invoice.total',
+            'charge' => 'required',
+            'notes',
+            'attachtment',
+            'link' => 'required'
+
+        ]);
+        
+
+        if ($validator->fails()) {
+            throw new InvalidArgumentException($validator->errors()->first());
+        }
+
+        $result = $this->instructionRepository->update($id,$data);
+
+        return $result;
+    }
+
+    public function getAll()
+    {
+        return $this->instructionRepository->getAll();
     }
 
     public function terminateInstruction($data, $id){
